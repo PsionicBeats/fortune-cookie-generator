a2cad5662d3d3cd6a7e7c3a4ce16a6c8c2294f7f
Alex Aguinaldo <psionicbones@gmail.com>
2017-01-12 23:12:16 -1000

    update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
 console.log("hi");
 
 var fortunesList = ["Confidence is a flimsy shield.", "Welcome adversity.", "The early bird gets the worm, but the second mouse gets the cheese."];
-var text = "";
-var i = Math.floor(Math.random() * 3);
-	text = fortunesList[i];
-	
-function generateFortuneCookie() {
-	document.getElementById('fortune-cookie-text').innerHTML = fortunesList[0];
-}
+//if any fortunes are added to the list above, make sure to change "for loop" paramter one value (var i = " ";) and stats function at bottom
+function generateFortuneCookie(){ //runs for the first time button is pressed
+  var cloneList = fortunesList.slice(); //this will console all values within the array, allowing fortunesList to stay as a global variable
+
+  //randomizer for fortunes
+  var randomFortune = " ";
+  for (var i = 2; i >= 0; i--){       
+    randomFortune = cloneList.splice(Math.floor(Math.random() * (i + 1)), 1); 
+    //(i + 1) 
+    console.log(randomFortune);
+    //temporarily stores random list
+    var tempCache = document.getElementById("fortune-cache");
+    var nodeone = document.createElement("DIV");
+    nodeone.innerText = randomFortune;
+    tempCache.appendChild(nodeone);
+  }
+
+  //changes button to prevent a new list of variables from being created
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "block";
+
+  //appends last fortune from "fortune-cache" into "fortune-cookie-text"
+  var cookieText = document.getElementById("fortune-cookie-text");
+  var nodetwo = tempCache.lastChild;
+  cookieText.appendChild(nodetwo);
